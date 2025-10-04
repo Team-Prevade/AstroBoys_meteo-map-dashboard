ce6ee05200993b46f7e6b995b12ffed38e174d3c
vanilsondacosta <cinzaelithe@gmail.com>
2025-10-04 12:09:25 +0100

    leve alterção na responsividade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
-import NasaLogo from '../assets/logo/NASA_logo.width-580.png';
-import SearchBar from "./tools/Searchbar"
+import NasaLogo from "../assets/logo/NASA_logo.width-580.png";
+import SearchBar from "./tools/Searchbar";
+
 export default function Header() {
   return (
-    <header className="flex gap-60  items-center justify-between px-6 py-4 bg-gray-900 text-white shadow-md">
-      <div className="flex items-center space-x-3">
-        <img src={NasaLogo} alt="NASA Logo" className="w-10 h-10"/>
-        <h1 className="text-xl font-bold">NASA Apps Challenge — MeteoMap</h1>
+    <header className="flex flex-col md:flex-row items-center justify-between px-4 py-3 bg-gray-900 text-white shadow-md gap-4">
+     
+      <div className="flex items-center space-x-3 text-center md:text-left">
+        <img src={NasaLogo} alt="NASA Logo" className="w-10 h-10" />
+        <h1 className="text-lg md:text-xl font-bold leading-tight">
+          NASA Apps Challenge — MeteoMap
+        </h1>
       </div>
-      <div className={"flex w-screen"}>
+      
+{/* vanilson */}
+      
+      <div className="w-full md:w-auto">
         <SearchBar />
       </div>
     </header>
