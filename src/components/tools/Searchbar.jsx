87ada817f4f63a75577ecb0a7195b02fd9e4157a
Dejanir Martins <dejanir.diogo@gmail.com>
2025-10-04 13:00:59 +0100

    Adiciona o componente Searchbar com funcionalidade de busca e integra react-icons

--- src/components/tools/Searchbar.jsx
+++ src/components/tools/Searchbar.jsx
@@ -1,11 +1,29 @@
-export default function Searchbar(){
+import { FaSearch} from "react-icons/fa";
+import {useState} from "react";
+
+export default function Searchbar({buscar}){
+  const [query, setQuery] = useState(""); 
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(buscar) buscar(query);// chama a funcao que vai buscar o local
+  };
+  
     return(
-        <>
-        <input
+        <div className="flex justify-center w-full">
+          <form onSubmit={handleSubmit} className="flex items-center bg-gray-700 rounded-md p-2">
+            <input
         type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
         placeholder="Pesquisar local..."
         className="px-3 py-2 rounded-md bg-gray-800 text-white focus:outline-none w-64"
       />
-        </>
+      <button type="submit">
+        <FaSearch className="text-white ml-2 " />
+      </button>
+          </form>
+        
+        </div>
     )
 }
